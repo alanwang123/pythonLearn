664a418b24f79eebd901a7dda4b1eab6ad7e1e02
alanwang123 <1727181@gmail.com>
2023-12-13 20:34:01 +0800

    Update parity.py

--- lesson2 conditionals/parity.py
+++ lesson2 conditionals/parity.py
@@ -1,23 +1,27 @@
 def main():
     x = int(input("what's x ? "))
     #偶数even判断
-    if is_even(x):
+    if is_even3(x):
         print("Even")
     #奇数odd判断
     else:
         print("odd")
 
 #定义奇偶判断的函数方式一
-def is_even(n):
+def is_even1(n):
     if n % 2 ==0:
         return True
     else:
         return False
 
 #定义奇偶判断的函数方式二
-def is_even(n):
+def is_even2(n):
     return True if n % 2 == 0  else False 
-    
+
+#定义奇偶判断的函数方式三
+def is_even3(n):
+    return ( n % 2 == 0)
+
     
 #执行主函数
 main()
